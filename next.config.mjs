282eaf240b852dc08044a229161f9ff3fea4b1c4
Mythhhhhhhh <32997045+Mythhhhhhhh@users.noreply.github.com>
2024-06-25 02:24:21 +0800

    feat:第一阶段完成抽奖部署 (#2)

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    target: 'server',
+    output: 'standalone',
     env: {
         target: 'server',
         output: "standalone",
         API_HOST_URL: process.env.API_HOST_URL
-    },
+    }
 }
 
 export default nextConfig;
